fc97a2b791cfd808159fbe7d6399bd1f739d2486
Jeff Ripke <jripke74@gmail.com>
2022-06-07 13:44:17 -0700

    add users to list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUserslist] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserslist((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
