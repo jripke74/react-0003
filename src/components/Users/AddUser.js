d84871a307173953cfc07ff207087fead465ec2c
Jeff Ripke <jripke74@gmail.com>
2022-06-08 08:54:20 -0700

    fix

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,13 +7,22 @@ import classes from './AddUser.module.css';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      })
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0)',
+      })
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -30,6 +39,7 @@ const AddUser = (props) => {
   };
 
   return (
+    {error && <ErrorModal title={error.title} message={error.message} />}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
